77792055c2145471e14fd5bfbda281340d52a2b9
Derwei Chan <dchan@pivotal.io>
2016-04-29 13:32:17 -0700

    Fix the redirect issue. Use HTTP headers instead
    Signed-off-by: Nima Kaviani <nkavian@us.ibm.com>

--- handlers/handler.go
+++ handlers/handler.go
@@ -20,6 +20,21 @@ func NewHandler(config config.Config) *handler {
 func (h *handler) GetMeta(writer http.ResponseWriter, request *http.Request) {
 		repoName := strings.Split(request.URL.Path, "/")[1]
 		writer.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+		// do not redirect if the agent is known from the NoRedirect list
+	  if !contains(h.config.NoRedirectAgents, request.Header.Get("User-Agent")){
+			repoPath := strings.TrimLeft(request.URL.Path, "/")
+			// if go-get=1 redirect to godoc.org
+			if request.URL.Query().Get("go-get") == "1" {
+				location := fmt.Sprintf("https://godoc.org/%s/%s", h.config.Host, repoPath)
+				http.Redirect(writer, request, location, http.StatusSeeOther)
+			} else {
+				location := h.config.OrgList[0] + repoPath
+				http.Redirect(writer, request, location, http.StatusMovedPermanently)
+			}
+			return
+		}
+
 		fmt.Fprintf(writer, "<meta name=\"go-import\" content=\"%s git %s\">",
 			h.config.Host + "/" + repoName,
 			h.config.OrgList[0] + repoName,
@@ -29,29 +44,11 @@ func (h *handler) GetMeta(writer http.ResponseWriter, request *http.Request) {
 			h.config.Host + "/" + repoName,
 			h.config.OrgList[0] + repoName,
 		)
-
-		// do not redirect if the agent is known from the NoRedirect list
-	  if contains(h.config.NoRedirectAgents, request.Header.Get("User-Agent")){
-			return
-		}
-
-		repoPath := strings.TrimLeft(request.URL.Path, "/")
-		// if go-get=1 redirect to godoc.org
-		if request.URL.Query().Get("go-get") == "1" {
-			fmt.Fprintf(writer, 
-				"<meta http-equiv=\"refresh\" content=\"0; url=https://godoc.org/%s/%s\">",
-				h.config.Host, repoPath)
-		} else {
-			fmt.Fprintf(writer, 
-				"<meta http-equiv=\"refresh\" content=\"0; url=%s\">",
-				h.config.OrgList[0] + repoPath)
-		}
 }
-
 
 func contains(slice []string, object string) bool {
 	for _, a := range slice {
-		if a == object {
+		if strings.Contains(object, a) {
 			return true
 		}
 	}
