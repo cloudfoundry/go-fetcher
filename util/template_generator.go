f1c6f9527b75f7cf416ef539b0b999d4645011ad
Gaurab Dey <gdey@pivotal.io>
2016-06-15 14:21:46 -0700

    adds template to recieve service names based on env
    [#121505915]
    
    Signed-off-by: Derwei Chan <dchan@pivotal.io>

--- util/template_generator.go
+++ util/template_generator.go
@@ -6,6 +6,10 @@ import (
 	"os"
 )
 
+type TemplateMapper struct {
+	APP_NAME, SERVICE_NAME string
+}
+
 func GenerateManifest(templatePath, targetPath string) error {
 	t, err := template.ParseFiles(templatePath)
 	if err != nil {
@@ -16,7 +20,13 @@ func GenerateManifest(templatePath, targetPath string) error {
 	if appName == "" {
 		return fmt.Errorf("APP_NAME is missing")
 	}
-	return generateActual(t, targetPath, appName)
+
+	serviceName := os.Getenv("SERVICE_NAME")
+	if serviceName == "" {
+		return fmt.Errorf("SERVICE_NAME is missing")
+	}
+	var appNameMapper = TemplateMapper{appName, serviceName}
+	return generateActual(t, targetPath, appNameMapper)
 }
 
 func GenerateConfig(templatePath, targetPath string) error {
@@ -26,19 +36,21 @@ func GenerateConfig(templatePath, targetPath string) error {
 	}
 	appName := os.Getenv("APP_NAME")
 	domain := os.Getenv("DOMAIN")
+	var appDomainMapper = TemplateMapper{appName + "." + domain, ""}
+
 	if appName == "" || domain == "" {
 		return fmt.Errorf("APP_NAME or DOMAIN is missing")
 	}
 
-	return generateActual(t, targetPath, appName+"."+domain)
+	return generateActual(t, targetPath, appDomainMapper)
 }
 
-func generateActual(template *template.Template, templatePath string, value string) error {
+func generateActual(template *template.Template, templatePath string, mappers TemplateMapper) error {
 	f, err := os.Create(templatePath)
 	if err != nil {
 		return err
 	}
-	err = template.Execute(f, value)
+	err = template.Execute(f, mappers)
 	if err != nil {
 		return err
 	}
