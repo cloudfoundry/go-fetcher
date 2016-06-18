f8296f8d53b9dd2de21dd75da66afffc9a6d1e6a
Jen Spinney <jen.spinney@hpe.com>
2016-06-17 17:54:20 -0700

    Implement updating of the cache every 10 minutes.
    [#121430771]
    
    Signed-off-by: Gaurab Dey <gdey@pivotal.io>

--- cache/cache_loader.go
+++ cache/cache_loader.go
@@ -2,15 +2,20 @@ package cache
 
 import (
 	"os"
+	"time"
 
 	"github.com/google/go-github/github"
+	"github.com/pivotal-golang/clock"
 	"github.com/tedsuo/ifrit"
 )
+
+const CacheUpdateInterval = 10 * time.Minute
 
 type cacheLoader struct {
 	repoService   RepositoriesService
 	orgs          []string
 	locationCache *LocationCache
+	clock         clock.Clock
 }
 
 //go:generate counterfeiter -o fakes/fake_repositories_service.go . RepositoriesService
@@ -18,15 +23,45 @@ type RepositoriesService interface {
 	ListByOrg(org string, opt *github.RepositoryListByOrgOptions) ([]github.Repository, *github.Response, error)
 }
 
-func NewCacheLoader(orgs []string, locationCache *LocationCache, repoService RepositoriesService) ifrit.Runner {
+func NewCacheLoader(orgs []string, locationCache *LocationCache, repoService RepositoriesService, clock clock.Clock) ifrit.Runner {
 	return &cacheLoader{
 		orgs:          orgs,
 		locationCache: locationCache,
 		repoService:   repoService,
+		clock:         clock,
 	}
 }
 
 func (c *cacheLoader) Run(signals <-chan os.Signal, ready chan<- struct{}) error {
+	err := c.updateCache()
+
+	// On starup, fail if there is an error with the initial call to github,
+	// becaue it's more likely to be noticed and there's a higher change the
+	// problem is with us. During the regular interval updates of the change, don't
+	// bring the process down if there's an error talking to github, as we expect
+	// it might just be temporary downtime for github.
+	if err != nil {
+		return err
+	}
+
+	close(ready)
+
+	timer := c.clock.NewTimer(CacheUpdateInterval)
+	for {
+		select {
+		case <-timer.C():
+			_ = c.updateCache()
+			// TODO: log error if non-nil
+			timer.Reset(CacheUpdateInterval)
+
+		case <-signals:
+			// TODO: log that we were signalled
+		}
+	}
+	return nil
+}
+
+func (c *cacheLoader) updateCache() error {
 	opt := &github.RepositoryListByOrgOptions{}
 	for _, org := range c.orgs {
 		for {
@@ -50,8 +85,5 @@ func (c *cacheLoader) Run(signals <-chan os.Signal, ready chan<- struct{}) error
 		}
 	}
 
-	close(ready)
-
-	<-signals
 	return nil
 }
--- cache/cache_loader_test.go
+++ cache/cache_loader_test.go
@@ -1,10 +1,13 @@
 package cache_test
 
 import (
+	"time"
+
 	"github.com/cloudfoundry/go-fetcher/cache"
 	"github.com/cloudfoundry/go-fetcher/cache/fakes"
 	"github.com/google/go-github/github"
 	"github.com/pivotal-golang/clock"
+	"github.com/pivotal-golang/clock/fakeclock"
 	"github.com/tedsuo/ifrit"
 
 	. "github.com/onsi/ginkgo"
@@ -16,12 +19,14 @@ var _ = Describe("CacheLoader", func() {
 		fakeRepoService *fakes.FakeRepositoriesService
 		cacheLoader     ifrit.Runner
 		locCache        *cache.LocationCache
+		fakeClock       *fakeclock.FakeClock
 	)
 
 	BeforeEach(func() {
 		fakeRepoService = &fakes.FakeRepositoriesService{}
+		fakeClock = fakeclock.NewFakeClock(time.Now())
 		locCache = cache.NewLocationCache(clock.NewClock())
-		cacheLoader = cache.NewCacheLoader([]string{"org1", "org2"}, locCache, fakeRepoService)
+		cacheLoader = cache.NewCacheLoader([]string{"org1", "org2"}, locCache, fakeRepoService, fakeClock)
 		fakeRepoService.ListByOrgReturns(nil, &github.Response{}, nil)
 	})
 
@@ -68,7 +73,7 @@ var _ = Describe("CacheLoader", func() {
 		})
 		fakeRepoService.ListByOrgReturns(returnedRepos, &github.Response{}, nil)
 
-		cacheLoader = cache.NewCacheLoader([]string{"http://github.com/org1/"}, locCache, fakeRepoService)
+		cacheLoader = cache.NewCacheLoader([]string{"http://github.com/org1/"}, locCache, fakeRepoService, fakeClock)
 		ifrit.Invoke(cacheLoader)
 		storedLocation, foundInCache := locCache.Lookup("repo1")
 
@@ -102,5 +107,17 @@ var _ = Describe("CacheLoader", func() {
 
 		// Expect 8 calls because we have 2 organizations, and each have 4 pages (0,1,2,3)
 		Expect(fakeRepoService.ListByOrgCallCount()).To(Equal(8))
+	})
+
+	It("updates the cache periodically", func() {
+		ifrit.Invoke(cacheLoader)
+
+		Expect(fakeRepoService.ListByOrgCallCount()).To(Equal(2))
+
+		fakeClock.WaitForWatcherAndIncrement(cache.CacheUpdateInterval)
+		Eventually(fakeRepoService.ListByOrgCallCount).Should(Equal(4))
+
+		fakeClock.WaitForWatcherAndIncrement(cache.CacheUpdateInterval)
+		Eventually(fakeRepoService.ListByOrgCallCount).Should(Equal(6))
 	})
 })
