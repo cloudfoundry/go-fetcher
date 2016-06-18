7f214f77e62f444de07783641914f6a58c1b4db5
Jen Spinney <jen.spinney@hpe.com>
2016-06-17 17:08:31 -0700

    Rename some internal fields in cache.go and change TTL to 15 minutes
    Signed-off-by: Gaurab Dey <gdey@pivotal.io>

--- cache/cache.go
+++ cache/cache.go
@@ -6,21 +6,22 @@ import (
 	"github.com/pivotal-golang/clock"
 )
 
-type item struct {
-	value   string
-	createdAt time.Time
+type cacheEntry struct {
+	// location is the full url to the repo on github
+	location  string
+	updatedAt time.Time
 }
 
 type LocationCache struct {
-	items map[string]*item
+	items map[string]*cacheEntry
 	clock clock.Clock
 }
 
-const CacheItemTTL = 60 * time.Second
+const CacheItemTTL = 15 * time.Minute
 
 func NewLocationCache(clock clock.Clock) *LocationCache {
 	return &LocationCache{
-		items: map[string]*item{},
+		items: map[string]*cacheEntry{},
 		clock: clock,
 	}
 }
@@ -28,8 +29,8 @@ func NewLocationCache(clock clock.Clock) *LocationCache {
 func (l *LocationCache) Lookup(repoName string) (string, bool) {
 	if item, ok := l.items[repoName]; !ok {
 		return "", false
-	} else if l.clock.Since(item.createdAt) <= CacheItemTTL {
-		return item.value, true
+	} else if l.clock.Since(item.updatedAt) <= CacheItemTTL {
+		return item.location, true
 	}
 
 	l.items[repoName] = nil
@@ -38,5 +39,5 @@ func (l *LocationCache) Lookup(repoName string) (string, bool) {
 }
 
 func (l *LocationCache) Add(repoName, location string) {
-	l.items[repoName] = &item{value: location, createdAt: l.clock.Now()}
+	l.items[repoName] = &cacheEntry{location: location, updatedAt: l.clock.Now()}
 }
--- cache/cache_test.go
+++ cache/cache_test.go
@@ -10,41 +10,61 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Lookup", func() {
+var _ = Describe("Location Cache", func() {
 	var locationCache *cache.LocationCache
 	var clock *fakeclock.FakeClock
 
-	BeforeEach(func() {
-		clock = fakeclock.NewFakeClock(time.Now())
-		locationCache = cache.NewLocationCache(clock)
-	})
+	Describe("Lookup", func() {
 
-	Context("when there is nothing in the cache", func() {
-		It("returns not ok", func() {
-			_, ok := locationCache.Lookup("something")
-			Expect(ok).To(BeFalse())
-		})
-	})
-
-	Context("when looking up a value that is in the cache", func() {
 		BeforeEach(func() {
-			locationCache.Add("repo-name", "cached-location")
+			clock = fakeclock.NewFakeClock(time.Now())
+			locationCache = cache.NewLocationCache(clock)
 		})
 
-		It("returns ok", func() {
-			_, ok := locationCache.Lookup("repo-name")
-			Expect(ok).To(BeTrue())
+		Context("when there is nothing in the cache", func() {
+			It("returns not ok", func() {
+				_, ok := locationCache.Lookup("something")
+				Expect(ok).To(BeFalse())
+			})
 		})
 
-		It("returns the cached location", func() {
-			location, _ := locationCache.Lookup("repo-name")
-			Expect(location).To(Equal("cached-location"))
-		})
+		Context("when looking up a value that is in the cache", func() {
+			BeforeEach(func() {
+				locationCache.Add("repo-name", "cached-location")
+			})
 
-		It("returns not ok after TTL expires", func() {
-			clock.Increment(cache.CacheItemTTL + 1)
-			_, ok := locationCache.Lookup("repo-name")
-			Expect(ok).To(BeFalse())
+			It("returns ok", func() {
+				_, ok := locationCache.Lookup("repo-name")
+				Expect(ok).To(BeTrue())
+			})
+
+			It("returns the cached location", func() {
+				location, _ := locationCache.Lookup("repo-name")
+				Expect(location).To(Equal("cached-location"))
+			})
+
+			It("returns not ok after TTL expires", func() {
+				clock.Increment(cache.CacheItemTTL + 1)
+				_, ok := locationCache.Lookup("repo-name")
+				Expect(ok).To(BeFalse())
+			})
+		})
+	})
+
+	Describe("Add", func() {
+		Context("when there is already an entry for the given key", func() {
+			BeforeEach(func() {
+				locationCache.Add("repo-name", "cached-location")
+			})
+
+			It("updates the TTL", func() {
+				clock.Increment(cache.CacheItemTTL - 1)
+				locationCache.Add("repo-name", "cached-location")
+
+				clock.Increment(cache.CacheItemTTL)
+				location, _ := locationCache.Lookup("repo-name")
+				Expect(location).To(Equal("cached-location"))
+			})
 		})
 	})
 })
