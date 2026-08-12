8e0497a9dd38466ee0152123868fc7fc7f4091d9
aram price <aram.price@broadcom.com>
2026-08-12 16:52:25 -0700

    Nit: specs use TempDir isntead of manual cleanup
    - rename vars for clarity

--- config/config_test.go
+++ config/config_test.go
@@ -2,6 +2,7 @@ package config_test
 
 import (
 	"os"
+	"path/filepath"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -10,18 +11,10 @@ import (
 )
 
 var _ = Describe("Load Configuration", func() {
-
-	var (
-		tmpDir   string
-		filePath string
-	)
+	var configFile string
 
 	BeforeEach(func() {
-		var err error
-		tmpDir, err = os.MkdirTemp("", "")
-		Expect(err).NotTo(HaveOccurred())
-
-		jsonContent := []byte(` {
+		jsonContent := []byte(`{
 		  "importPrefix": "test",
 		  "orgList": ["test_org"],
 		  "NoRedirectAgents": ["test_agent"],
@@ -34,25 +27,25 @@ var _ = Describe("Load Configuration", func() {
 		  "IndexPath": "some_relative/path"
 		}`)
 
-		err = os.WriteFile(tmpDir+"/config.json", jsonContent, 0644)
-		Expect(err).NotTo(HaveOccurred())
-		filePath = tmpDir + "/config.json"
-	})
+		configFile = filepath.Join(GinkgoT().TempDir(), "config.json")
 
-	AfterEach(func() {
-		err := os.RemoveAll(tmpDir)
-		Expect(err).NotTo(HaveOccurred())
+		Expect(os.WriteFile(configFile, jsonContent, 0644)).To(Succeed())
 	})
 
 	Context("when there is a config file", func() {
 		It("returns the parsed configuration", func() {
-			c, err := config.Parse(filePath)
+			parsedConfig, err := config.Parse(configFile)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(c.ImportPrefix).To(Equal("test"))
-			Expect(c.OrgList).To(Equal([]string{"test_org"}))
-			Expect(c.NoRedirectAgents).To(Equal([]string{"test_agent"}))
-			Expect(c.Overrides["test_repo"]).To(Equal(config.Override{Repository: "https://golang.example.com/test_repo", Path: "src/test_repo"}))
-			Expect(c.IndexPath).To(Equal("some_relative/path"))
+			Expect(parsedConfig.ImportPrefix).To(Equal("test"))
+			Expect(parsedConfig.OrgList).To(Equal([]string{"test_org"}))
+			Expect(parsedConfig.NoRedirectAgents).To(Equal([]string{"test_agent"}))
+			Expect(parsedConfig.Overrides["test_repo"]).To(Equal(
+				config.Override{
+					Repository: "https://golang.example.com/test_repo",
+					Path:       "src/test_repo",
+				},
+			))
+			Expect(parsedConfig.IndexPath).To(Equal("some_relative/path"))
 		})
 	})
 })
